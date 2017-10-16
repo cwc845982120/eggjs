42851693dbc22d1140b5d6a8353cbbaea27aa908
曹文成 <845982120@qq.com>
2017-10-16 16:18:28 +0800

    删除多余路由

--- app/router.js
+++ app/router.js
@@ -3,5 +3,4 @@
 module.exports = app => {
 	const interceptor = app.middlewares.interceptor();
 	app.post('/home', interceptor, app.controller.home.home);
-	app.post('/test', app.controller.home.home);
 };
